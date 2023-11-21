e89a9196003562f0525709e0309518816a58f224
Masum Hossain <masum14092@gmail.com>
2023-11-21 23:58:04 +0600

    java

--- app/src/main/java/com/haikalzain/inventorypro/utils/FileUtils.kt
+++ app/src/main/java/com/haikalzain/inventorypro/utils/FileUtils.kt
@@ -1,128 +1,127 @@
-package com.haikalzain.inventorypro.utils;
+package com.haikalzain.inventorypro.utils
 
-import android.content.Context;
-import android.os.Build;
-import android.os.Environment;
-
-import com.haikalzain.inventorypro.R;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import android.content.Context
+import android.os.Build
+import android.os.Environment
+import com.haikalzain.inventorypro.R
+import java.io.File
+import java.util.Arrays
 
 /**
  * Created by haikalzain on 6/01/15.
  */
-public class FileUtils {
-    public static boolean isFileNameValid(String fileName){ //Not perfect but will do
+object FileUtils {
+    fun isFileNameValid(fileName: String): Boolean { //Not perfect but will do
         //Dot also banned to make things easy
-        final String[] ReservedChars = {"|", "\\", "?", "*", "<", "\"", ":", ">", "."};
-
-
-        for(String c :ReservedChars){
-            if(fileName.contains(c)){
-                return false;
+        val ReservedChars = arrayOf("|", "\\", "?", "*", "<", "\"", ":", ">", ".")
+        for (c in ReservedChars) {
+            if (fileName.contains(c)) {
+                return false
             }
         }
-
-        return true;
+        return true
     }
 
-    public static File getTemplatesDirectory(Context context){
-        String FolderName=context.getString(R.string.app_name);
-
-        File dir = null;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
-        {
-            dir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) + File.separator + FolderName +"/templates");
-        }
-        else
-        {
-            dir = new File(Environment.getExternalStorageDirectory() + FolderName +"/templates");
+    fun getTemplatesDirectory(context: Context): File? {
+        val FolderName = context.getString(R.string.app_name)
+        var dir: File? = null
+        dir = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            File(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
+                    .toString() + File.separator + FolderName + "/templates"
+            )
+        } else {
+            File(
+                Environment.getExternalStorageDirectory().toString() + FolderName + "/templates"
+            )
         }
 
         // Make sure the path directory exists.
-        if (!dir.exists())
-        {
+        if (!dir.exists()) {
             // Make it, if it doesn't exit
-            boolean success = dir.mkdirs();
-            if (!success)
-            {
-                dir = null;
+            val success = dir.mkdirs()
+            if (!success) {
+                dir = null
             }
         }
-        return dir;
+        return dir
     }
 
-    public static File getSpreadsheetsDirectory(Context context){
-        String FolderName=context.getString(R.string.app_name);
-
-        File dir = null;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
-        {
-            dir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) + File.separator + FolderName +"/spreadsheets");
-        }
-        else
-        {
-            dir = new File(Environment.getExternalStorageDirectory() + File.separator + FolderName +"/spreadsheets");
+    fun getSpreadsheetsDirectory(context: Context): File? {
+        val FolderName = context.getString(R.string.app_name)
+        var dir: File? = null
+        dir = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            File(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
+                    .toString() + File.separator + FolderName + "/spreadsheets"
+            )
+        } else {
+            File(
+                Environment.getExternalStorageDirectory()
+                    .toString() + File.separator + FolderName + "/spreadsheets"
+            )
         }
 
         // Make sure the path directory exists.
-        if (!dir.exists())
-        {
+        if (!dir.exists()) {
             // Make it, if it doesn't exit
-            boolean success = dir.mkdirs();
-            if (!success)
-            {
-                dir = null;
+            val success = dir.mkdirs()
+            if (!success) {
+                dir = null
             }
         }
-        return dir;
+        return dir
     }
 
-    public static List<File> getTemplateFiles(Context context){
-        return new ArrayList<>(Arrays.asList(getTemplatesDirectory(context).listFiles()));
+    fun getTemplateFiles(context: Context): List<File> {
+        return ArrayList(
+            Arrays.asList(
+                *getTemplatesDirectory(context)!!
+                    .listFiles()
+            )
+        )
     }
 
-    public static List<File> getSpreadsheetFiles(Context context){
-        return new ArrayList<>(Arrays.asList(getSpreadsheetsDirectory(context).listFiles()));
+    fun getSpreadsheetFiles(context: Context): List<File> {
+        return ArrayList(
+            Arrays.asList(
+                *getSpreadsheetsDirectory(context)!!
+                    .listFiles()
+            )
+        )
     }
 
-    public static boolean deleteSpreadsheet(Context context, String fileName){
-        return new File(getSpreadsheetsDirectory(context), fileName).delete();
+    fun deleteSpreadsheet(context: Context, fileName: String?): Boolean {
+        return File(getSpreadsheetsDirectory(context), fileName).delete()
     }
 
-    public static boolean deleteTemplate(Context context, String fileName){
-        return new File(getTemplatesDirectory(context), fileName).delete();
+    fun deleteTemplate(context: Context, fileName: String?): Boolean {
+        return File(getTemplatesDirectory(context), fileName).delete()
     }
 
-    public static List<String> getFileNamesWithoutExt(List<File> files){
-        List<String> names = new ArrayList<>();
-        for(File f: files){
-            names.add(getFileNameWithoutExt(f));
+    fun getFileNamesWithoutExt(files: List<File>): List<String> {
+        val names: MutableList<String> = ArrayList()
+        for (f in files) {
+            names.add(getFileNameWithoutExt(f))
         }
-        return names;
+        return names
     }
 
-    public static List<String> getFileNames(List<File> files){
-        List<String> names = new ArrayList<>();
-        for(File f: files){
-            names.add(f.getName());
+    fun getFileNames(files: List<File>): List<String> {
+        val names: MutableList<String> = ArrayList()
+        for (f in files) {
+            names.add(f.name)
         }
-        return names;
+        return names
     }
 
-    public static String getFileNameWithoutExt(File file){
-        int last = file.getName().lastIndexOf(".");
-        return file.getName().substring(0, last);
+    fun getFileNameWithoutExt(file: File): String {
+        val last = file.name.lastIndexOf(".")
+        return file.name.substring(0, last)
     }
 
-    public static String getFileNameWithoutExt(String fileName){
-        int last = fileName.lastIndexOf(".");
-        return fileName.substring(0, last);
+    fun getFileNameWithoutExt(fileName: String): String {
+        val last = fileName.lastIndexOf(".")
+        return fileName.substring(0, last)
     }
-
-
-
 }
